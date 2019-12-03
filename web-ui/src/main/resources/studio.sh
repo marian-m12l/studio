5d2162d036ab5b8a591797f6fe57d4717eb079c6
albang <alban@garrigue.me>
2019-12-04 00:21:49 +0100

    Copy lunii libs before running web service

--- web-ui/src/main/resources/studio.sh
+++ web-ui/src/main/resources/studio.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+DOT_STUDIO="$HOME/.studio"
+LOCAL_LUNIITHEQUE=$HOME/.local/share/Luniitheque
+
+[ -e "lib/lunii-java-util.jar" ] || cp $LOCAL_LUNIITHEQUE/lib/lunii-java-util.jar lib/
+[ -e "lib/lunii-device-gateway.jar" ] || cp $LOCAL_LUNIITHEQUE/lib/lib/lunii-device-gateway.jar lib/
+[ -e "lib/lunii-device-wrapper.jar" ] || cp $LOCAL_LUNIITHEQUE/lib/lunii-device-wrapper.jar lib/
+[ -d $DOT_STUDIO/lib ] || mkdir -p $HOME/.studio/lib
 
 java -cp ${project.build.finalName}.jar:lib/*:~/.studio/lib/*:. io.vertx.core.Launcher run ${vertx.main.verticle}
