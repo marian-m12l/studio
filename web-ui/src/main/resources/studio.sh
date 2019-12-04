3916418db0214b664af539a6a3192d6eea27e90a
marian <marian.muller@serli.com>
2019-12-04 13:43:15 +0100

    make sure all .studio subdirectories are created + copy JARs to .studio/lib instead of local lib folder

--- web-ui/src/main/resources/studio.sh
+++ web-ui/src/main/resources/studio.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-DOT_STUDIO="$HOME/.studio"
+
+DOT_STUDIO=$HOME/.studio
 LOCAL_LUNIITHEQUE=$HOME/.local/share/Luniitheque
 
-[ -e "lib/lunii-java-util.jar" ] || cp $LOCAL_LUNIITHEQUE/lib/lunii-java-util.jar lib/
-[ -e "lib/lunii-device-gateway.jar" ] || cp $LOCAL_LUNIITHEQUE/lib/lib/lunii-device-gateway.jar lib/
-[ -e "lib/lunii-device-wrapper.jar" ] || cp $LOCAL_LUNIITHEQUE/lib/lunii-device-wrapper.jar lib/
-[ -d $DOT_STUDIO/lib ] || mkdir -p $HOME/.studio/lib
+# Make sure the .studio subdirectories exist
+if [ ! -d $DOT_STUDIO/db ]; then mkdir -p $DOT_STUDIO/db; fi
+if [ ! -d $DOT_STUDIO/lib ]; then mkdir -p $DOT_STUDIO/lib; fi
+if [ ! -d $DOT_STUDIO/library ]; then mkdir -p $DOT_STUDIO/library; fi
 
-java -cp ${project.build.finalName}.jar:lib/*:$HOME/.studio/lib/*:. io.vertx.core.Launcher run ${vertx.main.verticle}
+# Copy Luniistore JARs if needed
+if [ ! -e "$DOT_STUDIO/lib/lunii-java-util.jar" ]; then cp $LOCAL_LUNIITHEQUE/lib/lunii-java-util.jar $DOT_STUDIO/lib/; fi
+if [ ! -e "$DOT_STUDIO/lib/lunii-device-gateway.jar" ]; then cp $LOCAL_LUNIITHEQUE/lib/lunii-device-gateway.jar $DOT_STUDIO/lib/; fi
+if [ ! -e "$DOT_STUDIO/lib/lunii-device-wrapper.jar" ]; then cp $LOCAL_LUNIITHEQUE/lib/lunii-device-wrapper.jar $DOT_STUDIO/lib/; fi
+
+java -cp ${project.build.finalName}.jar:lib/*:$DOT_STUDIO/lib/*:. io.vertx.core.Launcher run ${vertx.main.verticle}
