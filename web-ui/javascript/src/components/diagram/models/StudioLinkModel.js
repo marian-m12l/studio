e44718532bc2c04daa6a3038902cffc5725868fc
marian <marian.muller@serli.com>
2019-12-04 08:54:23 +0100

    custom links with arrow heads

---
+++ web-ui/javascript/src/components/diagram/models/StudioLinkModel.js
@@ -0,0 +1,21 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { DefaultLinkModel } from '@projectstorm/react-diagrams-defaults';
+
+
+class StudioLinkModel extends DefaultLinkModel {
+
+    constructor(inversed) {
+        super({
+            type: 'studio'
+        });
+        this.inversed = inversed;
+    }
+
+}
+
+export default StudioLinkModel;
