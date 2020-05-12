9a72a4c0167c2188bc058caf8260e9a30e59448f
marian <marian.muller@serli.com>
2020-05-12 02:23:43 +0200

    remove netty dns resolver to avoid warnings

--- web-ui/src/main/resources/studio-windows.bat
+++ web-ui/src/main/resources/studio-windows.bat
@@ -12,4 +12,4 @@ if not exist %DOT_STUDIO%\library\* mkdir %DOT_STUDIO%\library
 copy %STUDIO_PATH%\agent\studio-agent-${project.version}-jar-with-dependencies.jar %DOT_STUDIO%\agent\studio-agent.jar
 copy %STUDIO_PATH%\agent\studio-metadata-${project.version}-jar-with-dependencies.jar %DOT_STUDIO%\agent\studio-metadata.jar
 
-java -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory -Dfile.encoding=UTF-8 -cp %STUDIO_PATH%/${project.build.finalName}.jar;%STUDIO_PATH%/lib/*;. io.vertx.core.Launcher run ${vertx.main.verticle}
+java -Dvertx.disableDnsResolver=true -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory -Dfile.encoding=UTF-8 -cp %STUDIO_PATH%/${project.build.finalName}.jar;%STUDIO_PATH%/lib/*;. io.vertx.core.Launcher run ${vertx.main.verticle}
