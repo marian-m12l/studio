61902c3729d7d5f7781ca49c00834432317ad295
marian <marian.muller@serli.com>
2020-05-25 13:49:26 +0200

    popup to ask whether "enriched" binary format should be enabled

--- web-ui/javascript/src/reducers/settings.js
+++ web-ui/javascript/src/reducers/settings.js
@@ -6,18 +6,24 @@
 
 import React from "react";
 
+import {
+    LOCAL_STORAGE_ANNOUNCE_OPTOUT,
+    LOCAL_STORAGE_ALLOW_ENRICHED_BINARY_FORMAT
+} from "../utils/storage";
+
+
 const initialState = {
-    announceOptOut: localStorage.getItem('announceOptOut') === 'true' || false,
-    allowEnriched: localStorage.getItem('allowEnrichedBinaryFormat') === 'true' || false
+    announceOptOut: localStorage.getItem(LOCAL_STORAGE_ANNOUNCE_OPTOUT) === 'true' || false,
+    allowEnriched: localStorage.getItem(LOCAL_STORAGE_ALLOW_ENRICHED_BINARY_FORMAT) === 'true' || false
 };
 
 const settings = (state = initialState, action) => {
     switch (action.type) {
         case 'SET_ANNOUNCE_OPTOUT':
-            localStorage.setItem('announceOptOut', action.announceOptOut);
+            localStorage.setItem(LOCAL_STORAGE_ANNOUNCE_OPTOUT, action.announceOptOut);
             return { ...state, announceOptOut: action.announceOptOut };
         case 'SET_ALLOW_ENRICHED':
-            localStorage.setItem('allowEnrichedBinaryFormat', action.allowEnriched);
+            localStorage.setItem(LOCAL_STORAGE_ALLOW_ENRICHED_BINARY_FORMAT, action.allowEnriched);
             return { ...state, allowEnriched: action.allowEnriched };
         default:
             return state
---
+++ web-ui/javascript/src/utils/storage.js
@@ -0,0 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+export const LOCAL_STORAGE_ANNOUNCE_OPTOUT = 'announceOptOut';
+export const LOCAL_STORAGE_ANNOUNCE_LAST_SHOWN = 'lastAnnounceShown';
+export const LOCAL_STORAGE_ALLOW_ENRICHED_BINARY_FORMAT = 'allowEnrichedBinaryFormat';
