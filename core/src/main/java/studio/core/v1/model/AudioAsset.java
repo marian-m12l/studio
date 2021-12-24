bdae23ed1bd61f41148be6733359ceb618421405
Kairoh <fake@kairoh.com>
2021-12-24 22:50:01 +0100

    Merge Assets definition

--- core/src/main/java/studio/core/v1/model/AudioAsset.java
+++ core/src/main/java/studio/core/v1/model/AudioAsset.java
@@ -6,32 +6,14 @@
 
 package studio.core.v1.model;
 
-public class AudioAsset implements Asset {
-
-    private String mimeType;
-    private byte[] rawData;
+public class AudioAsset extends Asset {
 
     public AudioAsset() {
+        super();
     }
 
     public AudioAsset(String mimeType, byte[] rawData) {
-        this.mimeType = mimeType;
-        this.rawData = rawData;
+        super(mimeType, rawData);
     }
 
-    public String getMimeType() {
-        return mimeType;
-    }
-
-    public void setMimeType(String mimeType) {
-        this.mimeType = mimeType;
-    }
-
-    public byte[] getRawData() {
-        return rawData;
-    }
-
-    public void setRawData(byte[] rawData) {
-        this.rawData = rawData;
-    }
 }
