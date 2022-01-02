a67f0754a21d346ac8ccc24706101e08370a5991
ofayau <fake@ofayau.com>
2022-01-02 13:50:03 +0100

    Update unit test for Archive Reading and Writing

--- core/src/test/java/studio/core/v1/reader/archive/ArchiveStoryPackReaderTest.java
+++ core/src/test/java/studio/core/v1/reader/archive/ArchiveStoryPackReaderTest.java
@@ -1,20 +1,24 @@
 package studio.core.v1.reader.archive;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import studio.core.v1.Constants;
+import studio.core.v1.model.ControlSettings;
 import studio.core.v1.model.StageNode;
 import studio.core.v1.model.StoryPack;
 import studio.core.v1.model.enriched.EnrichedNodeMetadata;
+import studio.core.v1.model.enriched.EnrichedNodeType;
+import studio.core.v1.model.enriched.EnrichedPackMetadata;
 import studio.core.v1.model.metadata.StoryPackMetadata;
 import studio.core.v1.writer.archive.ArchiveStoryPackWriter;
 
@@ -35,21 +39,29 @@ public class ArchiveStoryPackReaderTest {
         Path zipPath = classpathResource(zipName);
         System.out.println("zipPath: " + zipPath);
 
-        // v2
         long t1 = System.currentTimeMillis();
-        StoryPackMetadata meta2 = reader.readMetadata(zipPath);
+        StoryPackMetadata metaActual = reader.readMetadata(zipPath);
         long t2 = System.currentTimeMillis();
 
-        System.out.println("readMetadata:");
-        System.out.printf("v2 (%s ms) : %s\n", t2 - t1, meta2);
+        System.out.printf("readMetadata (%s ms) : %s\n", t2 - t1, metaActual);
 
-//        assertEquals(meta1.getUuid(), meta2.getUuid());
-//        assertEquals(meta1.getTitle(), meta2.getTitle());
-//        assertEquals(meta1.getVersion(), meta2.getVersion());
-//        assertEquals(meta1.getDescription(), meta2.getDescription());
-//        assertEquals(meta1.getSectorSize(), meta2.getSectorSize());
-//        assertEquals(meta1.getFormat(), meta2.getFormat());
-//        assertArrayEquals(meta1.getThumbnail(), meta2.getThumbnail());
+        StoryPackMetadata metaExpected = new StoryPackMetadata();
+        metaExpected.setFormat(Constants.PACK_FORMAT_ARCHIVE);
+        metaExpected.setVersion((short) 1);
+        metaExpected.setTitle("SimplifiedSamplePack");
+        metaExpected.setUuid("60f84e3d-8a37-4b4a-9e67-fc13daad9bb9");
+        metaExpected.setDescription("");
+        metaExpected.setSectorSize(null);
+        metaExpected.setThumbnail(null);
+
+        assertAll("StoryPackMetadata", //
+                () -> assertEquals(metaExpected.getUuid(), metaActual.getUuid()),
+                () -> assertEquals(metaExpected.getTitle(), metaActual.getTitle()),
+                () -> assertEquals(metaExpected.getVersion(), metaActual.getVersion()),
+                () -> assertEquals(metaExpected.getDescription(), metaActual.getDescription()),
+                () -> assertEquals(metaExpected.getSectorSize(), metaActual.getSectorSize()),
+                () -> assertEquals(metaExpected.getFormat(), metaActual.getFormat()),
+                () -> assertArrayEquals(metaExpected.getThumbnail(), metaActual.getThumbnail()));
     }
 
     @Test
@@ -57,32 +69,60 @@ public class ArchiveStoryPackReaderTest {
         Path zipPath = classpathResource(zipName);
         System.out.println("zipPath: " + zipPath);
 
-        // v1
         long t1 = System.currentTimeMillis();
-        StoryPack sp1;
-        try (InputStream is = Files.newInputStream(zipPath)) {
-            sp1 = reader.read(is);
-        }
+        StoryPack spActual = reader.read(zipPath);
         long t2 = System.currentTimeMillis();
-        // v2
-        StoryPack sp2 = reader.read(zipPath);
-        long t3 = System.currentTimeMillis();
 
-        System.out.println("readStoryPack:");
-        System.out.printf("v1 (%s ms) : %s\n", t2 - t1, sp1);
-        System.out.printf("v2 (%s ms) : %s\n", t3 - t2, sp2);
+        System.out.printf("readStoryPack (%s ms) : %s\n", t2 - t1, spActual);
+
+        StoryPack spExpected = new StoryPack();
+        spExpected.setVersion((short) 1);
+        spExpected.setFactoryDisabled(false);
+        spExpected.setNightModeAvailable(false);
+        spExpected.setUuid("60f84e3d-8a37-4b4a-9e67-fc13daad9bb9");
+        spExpected.setEnriched(new EnrichedPackMetadata("SimplifiedSamplePack", ""));
+
+        List<StageNode> stages = Arrays.asList( //
+                new StageNode("60f84e3d-8a37-4b4a-9e67-fc13daad9bb9", null, null, null, null,
+                        new ControlSettings(false, false, false, false, false), //
+                        new EnrichedNodeMetadata("Pack selection stage", EnrichedNodeType.COVER, null, null)), //
+                new StageNode("0607ef7d-89db-4b09-9e7f-077d0c8c5690", null, null, null, null, //
+                        new ControlSettings(false, false, false, false, false), //
+                        new EnrichedNodeMetadata("Story: Alice + Jungle", EnrichedNodeType.STORY,
+                                "0607ef7d-89db-4b09-9e7f-077d0c8c5690", null)), //
+                new StageNode("c4aebf59-e53f-443e-8e52-cf1872181784", null, null, null, null, //
+                        new ControlSettings(false, false, false, false, false), //
+                        new EnrichedNodeMetadata("Story: Alice + City", EnrichedNodeType.STORY,
+                                "c4aebf59-e53f-443e-8e52-cf1872181784", null)), //
+                new StageNode("b6f86bdd-30ee-4ef7-a50d-5d9bab6c28e1", null, null, null, null, //
+                        new ControlSettings(false, false, false, false, false), //
+                        new EnrichedNodeMetadata("Story: Bob + Jungle", EnrichedNodeType.STORY,
+                                "b6f86bdd-30ee-4ef7-a50d-5d9bab6c28e1", null)), //
+                new StageNode("1ed3b560-7ec0-4b4a-8f63-555b508b2186", null, null, null, null, //
+                        new ControlSettings(false, false, false, false, false), //
+                        new EnrichedNodeMetadata("Story: Bob + Desert", EnrichedNodeType.STORY,
+                                "1ed3b560-7ec0-4b4a-8f63-555b508b2186", null))//
+        // etc...
+        );
+        spExpected.setStageNodes(stages);
 
         assertAll("StoryPack", //
-                () -> assertEquals(sp1.getUuid(), sp2.getUuid()),
-                () -> assertEquals(sp1.getVersion(), sp2.getVersion()),
-                () -> assertEquals(sp1.getEnriched().getTitle(), sp2.getEnriched().getTitle()),
-                () -> assertEquals(sp1.getEnriched().getDescription(), sp2.getEnriched().getDescription()),
-                () -> assertEquals(sp1.getStageNodes().size(), sp2.getStageNodes().size()) //
+                () -> assertEquals(spExpected.getUuid(), spActual.getUuid()),
+                () -> assertEquals(spExpected.getVersion(), spActual.getVersion()),
+                () -> assertEquals(spExpected.getEnriched().getTitle(), spActual.getEnriched().getTitle()),
+                () -> assertEquals(spExpected.getEnriched().getDescription(), spActual.getEnriched().getDescription())
+        // () -> assertEquals(spExpected.getStageNodes().size(),
+        // spActual.getStageNodes().size()) //
         );
 
-        for (int i = 0; i < sp1.getStageNodes().size(); i++) {
-            StageNode sn1 = sp1.getStageNodes().get(i);
-            StageNode sn2 = sp2.getStageNodes().get(i);
+        spActual.getStageNodes().forEach(s -> {
+            System.out.printf("%s %s %s %s \n", s.getUuid(), s.getEnriched().getType(), s.getEnriched().getGroupId(),
+                    s.getEnriched().getName());
+        });
+
+        for (int i = 0; i < spExpected.getStageNodes().size(); i++) {
+            StageNode sn1 = spExpected.getStageNodes().get(i);
+            StageNode sn2 = spActual.getStageNodes().get(i);
             assertAll("StageNode-" + i, //
                     () -> assertEquals(sn1.getUuid(), sn2.getUuid()));
             EnrichedNodeMetadata en1 = sn1.getEnriched();
@@ -92,26 +132,17 @@ public class ArchiveStoryPackReaderTest {
                         () -> assertEquals(en1.getType(), en2.getType(), "type"),
                         () -> assertEquals(en1.getGroupId(), en2.getGroupId(), "groupid"),
                         () -> assertEquals(en1.getName(), en2.getName(), "name")
-//                        () -> assertEquals(en1.getPosition().getX(), en2.getPosition().getX(), "x"),
-//                        () -> assertEquals(en1.getPosition().getY(), en2.getPosition().getY(), "y") //
                 );
             }
         }
 
         // ------------------
+        // write
 
-        // v1
         long tt1 = System.currentTimeMillis();
-        try (OutputStream is = Files.newOutputStream(zipPath.resolveSibling("output-v1.zip"))) {
-            writer.write(sp1, is);
-        }
+        writer.write(spExpected, zipPath.resolveSibling("output-SimplifiedSamplePack.zip"));
         long tt2 = System.currentTimeMillis();
-        // v2
-        writer.write(sp1, zipPath.resolveSibling("output-v2.zip"));
-        long tt3 = System.currentTimeMillis();
 
-        System.out.println("writeStoryPack:");
-        System.out.printf("v1 (%s ms)\n", tt2 - tt1);
-        System.out.printf("v2 (%s ms)\n", tt3 - tt2);
+        System.out.printf("writeStoryPack (%s ms)\n", tt2 - tt1);
     }
 }
