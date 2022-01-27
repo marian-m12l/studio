4d16bab82bee6c3af2c78faeebe64d236187e6ad
Kairoh <fake@kairoh.com>
2022-01-27 21:31:13 +0100

    Redefine constants

--- core/src/test/java/studio/core/v1/utils/SecurityUtilsTest.java
+++ core/src/test/java/studio/core/v1/utils/SecurityUtilsTest.java
@@ -3,7 +3,9 @@ package studio.core.v1.utils;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
 import java.security.NoSuchAlgorithmException;
 
 import org.junit.jupiter.api.Test;
@@ -19,7 +21,7 @@ class SecurityUtilsTest {
         long t1 = System.nanoTime();
         String actual = SecurityUtils.sha1Hex(a);
         long t2 = System.nanoTime();
-        System.out.printf("sha1Hex (%s ms) : %s -> %s\n", t2 - t1, s, actual);
+        System.out.printf("sha1Hex (%s ns) : %s -> %s\n", t2 - t1, s, actual);
 
         assertEquals(expected, actual, "sha1");
     }
@@ -32,22 +34,20 @@ class SecurityUtilsTest {
         long t1 = System.nanoTime();
         byte[] actual = SecurityUtils.decodeHex(s);
         long t2 = System.nanoTime();
-        System.out.printf("decodeHex (%s ms) : %s -> %s\n", t2 - t1, s, new String(actual));
+        System.out.printf("decodeHex (%s ns) : %s -> %s\n", t2 - t1, s, new String(actual));
 
         assertArrayEquals(expected, actual, "decodeHex");
     }
 
     @Test
-    void encodeHex() {
+    void encodeHex() throws IOException, URISyntaxException {
         String s = "Hello world";
         byte[] a = s.getBytes();
         String expected = "48656c6c6f20776f726c64";
-
         long t1 = System.nanoTime();
         String actual = SecurityUtils.encodeHex(a);
         long t2 = System.nanoTime();
-        System.out.printf("encodeHex (%s ms) : %s -> %s\n", t2 - t1, s, actual);
-
+        System.out.printf("encodeHex (%s ns) : %s -> %s\n", t2 - t1, s, "");
         assertEquals(expected, actual, "encodeHex");
     }
 
