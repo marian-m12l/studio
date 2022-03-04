b431160cd425416799aaa353232a2a7e3e0878d6
kairoh <3878594+kairoh@users.noreply.github.com>
2022-03-04 21:25:10 +0100

    Delete unused agent

--- web-ui/src/main/scripts/studio-windows.bat
+++ web-ui/src/main/scripts/studio-windows.bat
@@ -4,16 +4,8 @@ set STUDIO_PATH=%~dp0
 set DOT_STUDIO="%UserProfile%\.studio"
 
 :: Make sure the .studio subdirectories exist
-:: if not exist %DOT_STUDIO%\agent\* mkdir %DOT_STUDIO%\agent
 if not exist %DOT_STUDIO%\db\* mkdir %DOT_STUDIO%\db
 if not exist %DOT_STUDIO%\library\* mkdir %DOT_STUDIO%\library
-
-:: Copy agent and metadata JARs
-:: copy "%STUDIO_PATH%\agent\studio-agent-${project.version}-jar-with-dependencies.jar" %DOT_STUDIO%\agent\studio-agent.jar
-:: copy "%STUDIO_PATH%\agent\studio-metadata-${project.version}-jar-with-dependencies.jar" %DOT_STUDIO%\agent\studio-metadata.jar
-
-:: -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager
-:: -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory
 
 java -Dvertx.disableDnsResolver=true -Dfile.encoding=UTF-8 ^
  -cp "%STUDIO_PATH%/${project.build.finalName}.jar";"%STUDIO_PATH%/lib/*";. ^
