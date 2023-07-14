f510de2d392f10f8a1939d9c1a1e83566e0c1fca
Horfee <horfee@gmail.com>
2023-07-14 16:12:49 +0200

    fix start script

--- studio-desktop/src/main/resources/studio.sh
+++ studio-desktop/src/main/resources/studio.sh
@@ -1,7 +1,12 @@
 #/bin/bash
 
 APP_PATH=`dirname $0`
-VERSION=studio-desktop-0.3.2-SNAPSHOT
+VERSION=studio-desktop-0.3.3-SNAPSHOT
 STUDIO_CP=$APP_PATH/lib/*:$APP_PATH:$APP_PATH/$VERSION.jar
 
-java -Xdock:description="Studio" -Xdock:icon=$APP_PATH/lunii.png -cp $STUDIO_CP -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager studio.GUI
+if [ "$(uname)" == "Darwin" ]; then
+    java -Xdock:description="Studio" -Xdock:icon=$APP_PATH/lunii.png -cp $STUDIO_CP -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager studio.GUI
+else; then
+    java -cp $STUDIO_CP -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager studio.GUI
+fi
+
