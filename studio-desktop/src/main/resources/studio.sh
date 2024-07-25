9c0e590202fa0bc49ca21b498f7b6ef9295c941d
horfee <horfee@gmail.com>
2024-07-25 10:15:41 +0200

    fix launch script

--- studio-desktop/src/main/resources/studio.sh
+++ studio-desktop/src/main/resources/studio.sh
@@ -1,12 +1,12 @@
-#/bin/bash
+#!/bin/bash
 
 APP_PATH=`dirname $0`
-VERSION=studio-desktop-0.3.3-SNAPSHOT
+VERSION=${project.build.finalName}
 STUDIO_CP=$APP_PATH/lib/*:$APP_PATH:$APP_PATH/$VERSION.jar
 
 if [ "$(uname)" == "Darwin" ]; then
     java -Xdock:description="Studio" -Xdock:icon=$APP_PATH/lunii.png -cp $STUDIO_CP -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager studio.GUI
-else; then
+else
     java -cp $STUDIO_CP -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager studio.GUI
 fi
 
